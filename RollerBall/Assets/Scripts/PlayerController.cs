f389671419d3c509144d1ab04f52f4167747c72d
VChow <victorchow_507@hotmail.com>
2015-06-15 23:11:15 +0100

    Initial Commit
    Initial Commit with gitignore.

---
+++ RollerBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rb;
+	private const float JUMP = 250;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	/*
+	 * Called before rendering a frame.
+	 * Most of the game code goes here.
+	 */
+	void Update ()
+	{
+		HandleKeyPresses ();
+	}
+
+	/*
+	 * Called before performing any physics calculations.
+	 * All Physics Code goes here.
+	 */
+	void FixedUpdate ()
+	{
+
+		HandleMovement ();
+
+	}
+
+	void HandleMovement ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+
+	void HandleKeyPresses ()
+	{
+		if (Input.GetKeyDown ("space")) {
+			rb.AddForce (Vector3.up * JUMP);
+		}
+
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}

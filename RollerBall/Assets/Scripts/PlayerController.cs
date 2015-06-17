ddcfe2238f0b6cefba10e3c31f8afa2c76f5e989
VChow <victorchow_507@hotmail.com>
2015-06-17 23:14:29 +0100

    Added input handlers for mobile devices. Added comments to code.

--- RollerBall/Assets/Scripts/PlayerController.cs
+++ RollerBall/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+#region Member Variables
 	public float speed;
 	public float accelerometerSpeed;
+
 	public Text countText;
 	public Text winText;
 	public Text debugLogger;
@@ -14,6 +16,7 @@ public class PlayerController : MonoBehaviour
 	private Rigidbody rb;
 	private const float JUMP = 250;
 	private int count;
+#endregion
 
 	void Start ()
 	{
@@ -22,16 +25,24 @@ public class PlayerController : MonoBehaviour
 		DisplayScore ();
 		winText.text = "";
 
+		//Set screen orientation to portrait for Android devices.
 		Screen.orientation = ScreenOrientation.Portrait;
+
 	}
-	
+
+#region Updates
 	/*
 	 * Called before rendering a frame.
 	 * Most of the game code goes here.
 	 */
 	void Update ()
 	{
-		InputController ();
+		if (SystemInfo.deviceType == DeviceType.Desktop) {
+			WindowsInputController ();
+		} else {
+			AndroidInputController ();	
+		}
+
 	}
 
 	/*
@@ -43,11 +54,15 @@ public class PlayerController : MonoBehaviour
 		if (SystemInfo.deviceType == DeviceType.Desktop) {
 			WindowsMovementController ();
 		} else {
-			AndroidMovementController ();
-	
+			AndroidMovementController ();	
 		}
 	}
+#endregion
 
+#region Movement Controllers
+	/*
+	 * Handles movement input for PC.
+	 */
 	void WindowsMovementController ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -58,48 +73,31 @@ public class PlayerController : MonoBehaviour
 		rb.AddForce (movement * speed);
 	}
 
+	/*
+	 * Handles axis/movement input for Android devices. 
+	 */
 	void AndroidMovementController ()
 	{
-		//Vector3 movement = Vector3.zero;
-		//dir.x = -Input.acceleration.y;
-		//dir.z = Input.acceleration.x;
-
-		//movement.x = Input.acceleration.x; //Left/Right working!
-		//movement.z = -Input.acceleration.y; //extreme tilt down, causes bouncing.
-
-		//movement.y = -Input.acceleration.y; //bounces (tilting down towards causes jumping, tilting away stops bouncing.
-		//movement.y = Input.acceleration.z; //No movement. can jump if jumping the phone.
-		//movement.y = Input.acceleration.y; doesnt move. occasionally small bounces.
-		//movement.y = -Input.acceleration.z; Bounces stationary
-
-		//movement.z = Input.acceleration.z; Bounces around, moves up down
-		//movement.z = -Input.acceleration.z; Bounces around, moves up down
-		//movement.z = -Input.acceleration.y; Does nothing
-		//movement.z = Input.acceleration.y; Randomly bouncing, does move
-
-		//if (movement.sqrMagnitude > 1)
-		//movement.Normalize ();
-
-		//movement *= Time.deltaTime;
-		//transform.Translate (movement * speed);
-
-		//===
 		float moveHorizontal = Input.acceleration.x;
 		float moveVertical = Input.acceleration.y;
+
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		
 		rb.AddForce (movement * accelerometerSpeed);
 
-		//===
-
-		debugLogger.text = "X: " + movement.x + " Y: " + movement.y + " Z: " + Input.acceleration.z;
+		//debugLogger.text = "X: " + movement.x + " Y: " + movement.y + " Z: " + Input.acceleration.z;
 	}
-	
-	void InputController ()
+#endregion
+
+#region Key/Touch Input Controllers
+	/*
+	 * Handles key input for PC.
+	 */ 
+	void WindowsInputController ()
 	{
 		//Space - Jump
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			//rb.AddForce (Vector3.up * JUMP);
+			rb.AddForce (Vector3.up * JUMP);
 		}
 
 		//R - Reset
@@ -107,22 +105,39 @@ public class PlayerController : MonoBehaviour
 			rb.MovePosition (new Vector3 (0, 0, 0));
 			rb.isKinematic = true;
 			rb.isKinematic = false;
-
 		}
-
 	}
 
+	/*
+	 * Handles touch input for Android.
+	 */ 
+	void AndroidInputController ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			rb.AddForce (Vector3.up * JUMP);
+		}
+	}
+#endregion
+
+
+
+	/*
+	 * Called when player hits a pick-up item.
+	 */ 
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
 
-			//Incremenet score
+			//Increment score
 			count ++;
 			DisplayScore ();
 		}
 	}
 
+	/*
+	 * Display and update the score.
+	 */ 
 	void DisplayScore ()
 	{
 		countText.text = "Count: " + count.ToString ();

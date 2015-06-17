e47c733997c5fe85c7ca5d17f0041d35dd9b4e28
VChow <victorchow_507@hotmail.com>
2015-06-17 22:47:59 +0100

    Added axis input control for Android devices.

--- RollerBall/Assets/Scripts/PlayerController.cs
+++ RollerBall/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,14 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
+	public float accelerometerSpeed;
 	public Text countText;
 	public Text winText;
+	public Text debugLogger;
 
 	private Rigidbody rb;
 	private const float JUMP = 250;
@@ -18,8 +21,10 @@ public class PlayerController : MonoBehaviour
 		count = 0;
 		DisplayScore ();
 		winText.text = "";
-	}
 
+		Screen.orientation = ScreenOrientation.Portrait;
+	}
+	
 	/*
 	 * Called before rendering a frame.
 	 * Most of the game code goes here.
@@ -35,12 +40,15 @@ public class PlayerController : MonoBehaviour
 	 */
 	void FixedUpdate ()
 	{
-
-		MovementController ();
-
+		if (SystemInfo.deviceType == DeviceType.Desktop) {
+			WindowsMovementController ();
+		} else {
+			AndroidMovementController ();
+	
+		}
 	}
 
-	void MovementController ()
+	void WindowsMovementController ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -50,11 +58,48 @@ public class PlayerController : MonoBehaviour
 		rb.AddForce (movement * speed);
 	}
 
+	void AndroidMovementController ()
+	{
+		//Vector3 movement = Vector3.zero;
+		//dir.x = -Input.acceleration.y;
+		//dir.z = Input.acceleration.x;
+
+		//movement.x = Input.acceleration.x; //Left/Right working!
+		//movement.z = -Input.acceleration.y; //extreme tilt down, causes bouncing.
+
+		//movement.y = -Input.acceleration.y; //bounces (tilting down towards causes jumping, tilting away stops bouncing.
+		//movement.y = Input.acceleration.z; //No movement. can jump if jumping the phone.
+		//movement.y = Input.acceleration.y; doesnt move. occasionally small bounces.
+		//movement.y = -Input.acceleration.z; Bounces stationary
+
+		//movement.z = Input.acceleration.z; Bounces around, moves up down
+		//movement.z = -Input.acceleration.z; Bounces around, moves up down
+		//movement.z = -Input.acceleration.y; Does nothing
+		//movement.z = Input.acceleration.y; Randomly bouncing, does move
+
+		//if (movement.sqrMagnitude > 1)
+		//movement.Normalize ();
+
+		//movement *= Time.deltaTime;
+		//transform.Translate (movement * speed);
+
+		//===
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * accelerometerSpeed);
+
+		//===
+
+		debugLogger.text = "X: " + movement.x + " Y: " + movement.y + " Z: " + Input.acceleration.z;
+	}
+	
 	void InputController ()
 	{
 		//Space - Jump
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			rb.AddForce (Vector3.up * JUMP);
+			//rb.AddForce (Vector3.up * JUMP);
 		}
 
 		//R - Reset
